b3f5d03b8bd77664dc16d41e4c4e96c9d9856b6e
Aidan Finn <aidan@Aidan-Finns-MacBook-Pro.local>
2011-03-08 23:11:23 +0000

    Don't show deleted messages in unread message count

--- lib/has_private_messages_extensions.rb
+++ lib/has_private_messages_extensions.rb
@@ -48,7 +48,7 @@ module Professionalnerd #:nodoc:
         
         # Returns the number of unread messages for this user
         def unread_message_count
-          eval options[:class_name] + '.count(:conditions => ["recipient_id = ? AND read_at IS NULL", self])'
+          eval options[:class_name] + '.count(:conditions => ["recipient_id = ? AND read_at IS NULL and recipient_deleted = ?", self, false])'
         end
       end 
     end
